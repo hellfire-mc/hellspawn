7ae38821f9781504f2115013cb0a49606522b32f
kaylendog (Kaylen) <actuallyori@gmail.com>
2022-07-10 20:08:13 +0100

    feat: add kotlinGradle spotless config

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,18 @@
+/* Copyright (c) 2022 - Kaylen Dart and contributors. All rights reserved.
+ *
+ * This file is part of Hellfire Hellspawn.
+ *
+ * Hellfire Hellspawn is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Hellfire Hellspawn is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with Hellfire Hellspawn. If not, see
+ * <https://www.gnu.org/licenses/>.
+ */
 plugins {
     java
     `java-gradle-plugin`
@@ -20,6 +35,10 @@ spotless {
     kotlin {
         ktlint()
         licenseHeaderFile(rootProject.file("LICENSE_HEADER"))
+    }
+    kotlinGradle {
+        ktlint()
+        licenseHeaderFile(rootProject.file("LICENSE_HEADER"), "plugins")
     }
     format("styling") {
         target("**/*.json", "**/*.md")
